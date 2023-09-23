2311fa168e0a4eb1719e15f137235ae099819937
Abuemkeze Chu <abuemkeze@gmail.com>
2023-09-23 20:03:18 +0100

    feat: calculate statistics

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItems={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   )
 }
@@ -113,10 +113,26 @@ function Item({ item, onDeleteItem, onToggleItems }) {
   )
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>🧳 You have no items on your list, add some!</em>
+      </footer>
+    )
+
+  const numItems = items.length
+  const numPackedItems = items.filter((item) => item.packed).length
+  const percentage = Math.round((numPackedItems / numItems) * 100)
+
   return (
     <footer className="stats">
-      <em> 🧳 You have X items on your list, and you already packed x (x% )</em>
+      <em>
+        {percentage === 100
+          ? 'You get everything, enjoy your trip! 🛫'
+          : `🧳 You have ${numItems} items on your list, and you already packed
+        ${numPackedItems} (${percentage + '%'})`}
+      </em>
     </footer>
   )
 }
